aed652664dbbb28087149dbd230aedd055db411c
harshit <harshitkumarsingh203@gmail.com>
2025-09-29 22:45:32 +0530

    solved

---
+++ assignment3/search_in_2D_matrix.java
@@ -0,0 +1,21 @@
+package assignment3;
+
+public class search_in_2D_matrix {
+    class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int m = arr.length;
+        int n = arr[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j] == target) return true;
+            }
+
+        }
+
+        return false;
+
+        
+    }
+}
+    
+}
