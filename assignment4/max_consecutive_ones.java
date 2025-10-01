5ccd786ace41a6406ff44c0a3ca32dcae43c89ab
harshit <harshitkumarsingh203@gmail.com>
2025-10-01 14:08:28 +0530

    solved

--- assignment4/max_consecutive_ones.java
+++ assignment4/max_consecutive_ones.java
@@ -1,5 +1,34 @@
 package assignment4;
 
 public class max_consecutive_ones {
+    class Solution {
+    public int longestOnes(int[] nums, int k) {
+        
+        
+        int max = 0;
+        int n = nums.length;
+        int start = 0;
+        int end = 0;
+        int zero = 0;
+        while(end < n){
+            if(nums[end] == 0){
+                zero++;
+            }
+            end++;
+            while(start < end && zero > k){
+                if(nums[start] == 0){
+                    zero--;
+                }
+                start++;
+            }
+
+            max = Math.max(max , end - start);
+            
+        }
+
+        return max;
+        
+    }
+}
     
 }
