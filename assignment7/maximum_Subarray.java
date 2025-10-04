82a5d7b1aeb9007decc2522e150ba19e0cc78672
harshit <harshitkumarsingh203@gmail.com>
2025-10-04 23:32:04 +0530

    killed

---
+++ assignment7/maximum_Subarray.java
@@ -0,0 +1,22 @@
+public class maximum_Subarray {
+    class Solution {
+    public int maxSubArray(int[] arr) {
+
+        int sum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > maxsum) {
+                maxsum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
+
+    
+}
